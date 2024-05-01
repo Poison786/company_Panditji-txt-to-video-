6c1dfbfcd187ad4889ab13b8e1baab804124d7b5
Poison786 <168622740+Poison786@users.noreply.github.com>
2024-05-01 12:35:33 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,3 +1,3 @@
-api_id = "25934897"
-api_hash = "7c008f23e9c40465c39f509715d302ab"
-bot_token = "7159818308:AAGXykp5SNFk2Y9vS0Pu8OumdqhRhI7GmkA"
+api_id = "24119778"
+api_hash = "cca11ca97dd8683d65ca1beb62baceb1"
+bot_token = "7131049874:AAEPwBuu_Z5_gceCqjqXB2QH8KUA3RWyeM4"
